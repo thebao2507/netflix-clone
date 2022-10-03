7a783fc1a29ef501b736688f74624d1e01ca67cf
baobao <lethebao2507@gmail.com>
2022-10-03 23:03:53 +0700

    clone-netflix-day-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '5a2c528f7f370496ab1e44229bb9ded9';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;
